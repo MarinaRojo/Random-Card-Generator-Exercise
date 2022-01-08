56697bd75a6cabffe312f7482fa6d4d35652cf63
Marina Rojo <marinarojo.cc@gmail.com>
2022-01-08 11:08:10 +0000

    Random Card Generator Exercise

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let randomSuit = Math.floor(Math.random() * 4);
+  let number = Math.floor(Math.random() * 12 + 1);
+  const card = document.querySelector(".card");
+  let suit = suits[randomSuit];
+  if (number == 10) {
+    number = "J";
+  } else if (number == 11) {
+    number = "Q";
+  } else if (number == 12) {
+    number = "K";
+  }
+  card.innerHTML = `<div class="${suit} text-start">${suit}</div>
+                  <div class="number text-center">${number}</div>
+                  <div class="${suit} text-end">${suit}</div>
+  `;
 };
